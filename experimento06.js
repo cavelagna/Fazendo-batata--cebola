d339992369b45f840980517b55f10f066acbcebf
cavelagna <moreira.giovanni@escola.pr.gov.br>
2026-08-27 11:19:18 +0000

    experimento06 add modal .innerHTML

--- experimento06.js
+++ experimento06.js
@@ -2,5 +2,16 @@ const botaoAbrir = document.getElementById("btnAbrirModal");
 const modalContainer = document.getElementById("modalContainer");
 
 botaoAbrir.addEventListener("click", function() {
-	modalContainer.textContent = "Este é um aviso.";
+	modalContainer.innerHTML = `
+		<div class="modal-fundo">
+			<div class="modal-caixa">
+				<h2>Aviso!</h2>
+				<p> Este modal inteiro foi criado com JarvisScript.</p>
+				<button id = "btnFecharModal">Fecha</button>
+			</div>
+		</div>`;
+		const botaoFechar = document.getElementById("btnFecharModal");
+		botaoFechar.addEventListener("click",function(){
+			modalContainer.innerHTML = "";
+		})
 });
